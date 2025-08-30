9c252754e8e7e73f1ffdfc034fe181294b4287d5
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-08-30 12:14:29 +0200

    Workflow sandbox #5

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Download CoeJoder/waitForKeyElements.js
 git clone https://github.com/CoeJoder/waitForKeyElements.js.git git_waitForKeyElements
 
-ls -l
+# Download jquery/jquery
+npm i jquery
 
+
+
+ls -l
+ls -l git_waitForKeyElements
+ls -l node_modules/jquery
