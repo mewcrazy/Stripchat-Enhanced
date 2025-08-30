532ecee7261f21d77c587173681cca9a1928d76b
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-08-30 12:03:35 +0200

    Workflow sandbox #2

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! "wget -O waitForKeyElements.js https://gist.github.com/raw/2625891/waitForKeyElements.js"; then
+if ! 'wget -O waitForKeyElements.js "https://gist.github.com/raw/2625891/waitForKeyElements.js"'; then
   echo "ERROR: can't get archive" >&2
   exit 1
 fi
