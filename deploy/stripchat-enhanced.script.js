8deca2fd6fe9e72e1d5b01de21fc4d4fbf64f049
mewcrazy <kontakt@dennis-bitsch.de>
2025-09-06 03:44:16 +0000

    Update repository files [skip ci]

--- deploy/stripchat-enhanced.script.js
+++ deploy/stripchat-enhanced.script.js
@@ -67,7 +67,7 @@
 
 
     /* A Google API Key (for the Cloud Translation API) is needed to get this script to work */
-    var googleApiKey = "AIzaSyA8m0bay1Sg545_mrZKkmEFIh5bJw7A4a8";
+    var googleApiKey = "";
     var prefTranslationLang = localStorage.getItem("prefTranslationLang")
     var translationLanguages = []
 
