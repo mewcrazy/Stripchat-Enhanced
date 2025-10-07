1ec2728488bca84e18b64b8c940ce3894fd2f521
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-10-08 00:39:30 +0200

    Firefox Plugin Deployment Test #2

--- build/build.sh
+++ build/build.sh
@@ -12,4 +12,6 @@ mv -f ../../node_modules/jquery/dist/jquery.min.js deploy/jquery.min.js
 /usr/local/bin/crx3 extensions/chrome -o extensions/chrome.crx -p /home/githubrunner/extension_keys/chrome.pem
 
 # Build Firefox Extension File
-/usr/local/bin/web-ext build -o -s extensions/chrome -a extensions -n firefox.zip
+/usr/local/bin/web-ext build -o -s extensions/chrome -a ./extensions -n firefox.zip
+
+# Build Opera Extension File
