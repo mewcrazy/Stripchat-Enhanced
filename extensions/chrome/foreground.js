c1ab9d507e4f9b0da2a20b8664b0422dca5c66b9
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-10-12 21:43:09 +0200

    Improve "Model Info on Model Pages"

--- extensions/chrome/foreground.js
+++ extensions/chrome/foreground.js
@@ -347,25 +347,28 @@ function hideFavoritesFromFeaturedListings(el) {
     navLeft.find('.view-cam-header-sub__fan-club-button').addClass('hidden')
 
   }
-  waitForKeyElements(".view-cam-page .nav-right", addModelInfo, false);
+  waitForKeyElements(".view-cam-page .viewcam-profile-menu-item", addModelInfo, false);
   function addModelInfo(el) {
 
     // get api data
-    let username = $('.viewcam-profile-menu-item .viewcam-profile-menu-item__label').eq(0).text()
-    let navLeftItems = $(el).closest('div').find('.nav-left .visible-items')
-    $.getJSON('/api/front/v2/models/username/'+username+'/cam').done((data) => {
-      $('#body').addClass('se-category-info-processed')
+    if(!$('#body').hasClass('se-category-info-processed')) {
+      let username = $(el).find('.viewcam-profile-menu-item__label').eq(0).text()
+      let navLeftItems = $(el).parent()
+      console.log(navLeftItems)
+      $.getJSON('/api/front/v2/models/username/'+username+'/cam').done((data) => {
 
-      let langsHtml = ""
-      $.each(data.user.user.languages, function(k, v) {
-        langsHtml += '<span class="country-flag" data-lang="'+v+'" style="background-image: url(&quot;//mewcrazy.github.io/Stripchat-Enhanced/flags/'+v+'.svg&quot;);"></span>'
-      })
-      navLeftItems.append('<div class="header-sub-item-wrapper se-info-more"><span class="flex"><img src="https://web.static.mmcdn.com/images/ico-'+data.user.user.contestGender+'.svg">'+langsHtml+'</span><span title="'+data.user.user.birthDate+'"><svg class="icon icon-best-models"><use xlink:href="#icons-best-models"></use></svg>'+(data.user.user.age ? data.user.user.age+' years old' : 'no age given')+'</span></div>')
-      navLeftItems.append('<div class="header-sub-item-wrapper se-info-more"><span title="StripRank"><svg class="icon icon-best-models"><use xlink:href="#icons-best-models"></use></svg>#'+data.user.currPosition+'</span><span title="StripPoints"><svg class="icon icon-best-models"><use xlink:href="#icons-stripchat-logo"></use></svg>'+data.user.currPoints+'</span></div>')
-      navLeftItems.append('<div class="header-sub-item-wrapper se-info-more pvt">'+data.user.user.privateRate+' pvt/min.'+(data.user.user.ratingPrivate ? '<br><a href="/Cumonmme/profile"><span class="stars">'+data.user.user.ratingPrivate+'</span></a></div>' : ''))
-      navLeftItems.append('<div class="header-sub-item-wrapper se-info-more">'+data.user.user.p2pRate+' p2p/min. - '+data.user.user.spyRate+' spy/min.</div>')
-      $('span.stars').stars();
-    });
+        let langsHtml = ""
+        $.each(data.user.user.languages, function(k, v) {
+          langsHtml += '<span class="country-flag" data-lang="'+v+'" style="background-image: url(&quot;//mewcrazy.github.io/Stripchat-Enhanced/flags/'+v+'.svg&quot;);"></span>'
+        })
+        navLeftItems.after('<div class="header-sub-item-wrapper se-info-more">'+data.user.user.p2pRate+' p2p/min. - '+data.user.user.spyRate+' spy/min.</div>')
+        navLeftItems.after('<div class="header-sub-item-wrapper se-info-more pvt">'+data.user.user.privateRate+' pvt/min.'+(data.user.user.ratingPrivate ? '<br><a href="/Cumonmme/profile"><span class="stars">'+data.user.user.ratingPrivate+'</span></a></div>' : ''))
+        navLeftItems.after('<div class="header-sub-item-wrapper se-info-more"><span title="StripRank"><svg class="icon icon-best-models"><use xlink:href="#icons-best-models"></use></svg>#'+data.user.currPosition+'</span><span title="StripPoints"><svg class="icon icon-best-models"><use xlink:href="#icons-stripchat-logo"></use></svg>'+data.user.currPoints+'</span></div>')
+        navLeftItems.after('<div class="header-sub-item-wrapper se-info-more"><span class="flex"><img src="https://web.static.mmcdn.com/images/ico-'+data.user.user.contestGender+'.svg">'+langsHtml+'</span><span title="'+data.user.user.birthDate+'"><svg class="icon icon-best-models"><use xlink:href="#icons-best-models"></use></svg>'+(data.user.user.age ? data.user.user.age+' years old' : 'no age given')+'</span></div>')
+        $('span.stars').stars();
+      });
+      $('#body').addClass('se-category-info-processed')
+    }
 
     // switch translation lang on country flag click
     $('.country-flag').on('click', function(e) {
