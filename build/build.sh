65ae682a117d44a92c4ec9930c7cf7bb4531b1fc
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-10-19 21:11:16 +0200

    Minify JavaScript Files // Prepare JavaScript Files Obfuscation

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Download npm Modules
+npm i jquery uglify-js
+npm audit fix --force
+
 # Download waitForKeyElements.js & move to ~/deploy/
 git clone https://github.com/CoeJoder/waitForKeyElements.js.git git_waitForKeyElements
 mv -f git_waitForKeyElements/waitForKeyElements.js deploy/waitForKeyElements.min.js
 
 # Download jquery/jquery & Move to directory
-npm i jquery
-npm audit fix --force
 rm extensions/chrome/js/jquery.min.js
 cp -rf ../../../../node_modules/jquery/dist/jquery.min.js deploy/jquery.min.js
+
+# Obfuscate JavaScript files
+# ./../../../../node_modules/javascript-obfuscator/bin/javascript-obfuscator extensions/chrome/foreground.js --output extensions/chrome/foreground-obfuscated.js
+# rm extensions/chrome/foreground.js
+
+# Minify JavaScript files
+./../../../../node_modules/uglifyjs/bin/uglifyjs --compress --mangle -- extensions/chrome/foreground.js -o extensions/chrome/foreground.min.js
 
 # Build .crx Chrome Extension File
 /usr/local/bin/crx3 extensions/chrome -o extensions/chrome.crx -p /home/githubrunner/extension_keys/chrome.pem
